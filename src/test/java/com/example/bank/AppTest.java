5d143ce20207aa9a83be7b0dfcf6eb2607e11f63
DChampigny <DChampigny@SSLW-01397.spinsys.com>
2018-01-23 18:13:40 -0500

    First Commit

---
+++ src/test/java/com/example/bank/AppTest.java
@@ -0,0 +1,37 @@
+package com.example.bank;
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+    	super(testName);
+    	double result=App.calculateSimpleInterest(10000,5,7);
+    	Assert.assertEquals("Test failed. ",350000.0,result); 
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
