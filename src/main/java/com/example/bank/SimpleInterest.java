2b72be5701b72bd3bd00e5c18bf941f16585d6f2
dougchampigny <dougchampigny@yahoo.com>
2018-01-25 11:49:15 -0500

    Update SimpleInterest.java

--- src/main/java/com/example/bank/SimpleInterest.java
+++ src/main/java/com/example/bank/SimpleInterest.java
@@ -1,7 +1,8 @@
 package com.example.bank;                                                                                                   public class SimpleInterest{                                                                                                     
 	
 public static double calculateSimpleInterest(double amount, double years, double rate_of_interest) 
-{
+{	
+    /*this project is built in Jenkins and receives posts to run builds*/
     double simple_interest;
     simple_interest = amount * years * rate_of_interest;
     
